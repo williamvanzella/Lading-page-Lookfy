17af35b552a55182fb7e855c616a511129702018
João Gabriel Dourado Cervo <gabrielcervo@MacBook-Air-de-Joao-3.local>
2024-06-05 15:23:44 -0300

    changed: reduced  images and minor code fixes

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -13,20 +13,17 @@ $(document).ready(function() {
 
         let activeSectionIndex = 0;
 
-        if (scrollPosition <= 0) {
-            header.css('box-shadow', 'none');
-        } else {
-            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1');
-        }
+        const boxShadow = scrollPosition <= 0 ? 'none' : '5px 1px 5px rgba(0, 0, 0, 0.1'
+        header.css('box-shadow', boxShadow);
 
         sections.each(function(i) {
             const section = $(this);
-            const sectionTop = section.offset().top - 96;
-            const sectionBottom = sectionTop+ section.outerHeight();
+            const sectionTop = section.offset().top - 350;
+            const sectionBottom = sectionTop + section.outerHeight();
 
             if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
                 activeSectionIndex = i;
-                return false;
+                return;
             }
         })
 
